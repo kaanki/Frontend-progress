069d14d46028e565fae2b74339bfc60049834f81
kaan keles <kaan.keles@mail.com>
2023-03-26 16:47:32 +0300

    js local storage

--- yzl-3505/19g-25-03-2023-classes-DOM/js/script.js
+++ yzl-3505/19g-25-03-2023-classes-DOM/js/script.js
@@ -83,24 +83,40 @@ const navigationLink = document.getElementById("navigation");
 const btnClear = document.getElementById("clear");
 const todoList = document.querySelector("ul");
 
+const input = document.getElementById("user-todo");
+const form = document.querySelector("form");
+
+const todoMaker = function (text) {
+  const todo = document.createElement("li");
+  todo.textContent = text;
+  todoList.appendChild(todo);
+};
+// const todosArray = [];
+let todosArray = localStorage.getItem("todos")
+  ? JSON.parse(localStorage.getItem("todos"))
+  : [];
+
+localStorage.setItem("todos", JSON.stringify(todosArray));
+const storage = JSON.parse(localStorage.getItem("todos"));
+for (let i = 0; i < storage.length; i++) {
+  todoMaker(storage[i])
+}
+
 btnClear.addEventListener("click", function () {
+  localStorage.clear();
+  todosArray = [];
   while (todoList.firstChild) {
     todoList.removeChild(todoList.firstChild);
   }
 });
 
-const todoMaker = function (text) {
-    const todo = document.createElement('li')
-    todo.textContent = text
-    todoList.appendChild(todo);
-};
 
-const input = document.getElementById("user-todo");
-const form = document.querySelector("form");
 form.addEventListener("submit", function (e) {
   e.preventDefault();
+  todosArray.push(input.value);
+  localStorage.setItem("todos", JSON.stringify(todosArray));
+  console.log(todosArray);
   todoMaker(input.value);
-  console.log(input.value);
   input.value = "";
 });
 
