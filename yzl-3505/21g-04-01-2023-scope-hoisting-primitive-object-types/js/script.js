63f6a15c679ea1d0804262772447fe49859417e7
kaan keles <kaan.keles@mail.com>
2023-04-01 17:56:40 +0300

    js hoisting, TDZ, This

--- yzl-3505/21g-04-01-2023-scope-hoisting-primitive-object-types/js/script.js
+++ yzl-3505/21g-04-01-2023-scope-hoisting-primitive-object-types/js/script.js
@@ -1,29 +1,77 @@
 //use scri
 "use strict";
 
-function yasHesap(dogumYili) {
-  const yas = 2040 - dogumYili;
-  function yasYazdir() {
-    let output = `${isim} ${dogumYili} dogumlu  ${yas} yasinda`;
-    console.log(output);
-    if (dogumYili >= 1981 && dogumYili <= 1996) {
-      var millenial = true;
-      // Dış kapsamdaki değişken ile aynı isme sahip değişken oluşturma
-      const isim = "Namık";
-      // Dış kapsamdaki değişkene yeniden atama
-      output = "yeni çıktı";
-      const str = `${isim} Y kusaginda dogmussun`;
-      console.log(str);
-      function ekle(a, b) {
-        return a + b;
-      }
-    }
-    console.log(output);
-  }
-  yasYazdir();
-  return yas;
+// function yasHesap(dogumYili) {
+//   const yas = 2040 - dogumYili;
+//   function yasYazdir() {
+//     let output = `${isim} ${dogumYili} dogumlu  ${yas} yasinda`;
+//     console.log(output);
+//     if (dogumYili >= 1981 && dogumYili <= 1996) {
+//       var millenial = true;
+//       // Dış kapsamdaki değişken ile aynı isme sahip değişken oluşturma
+//       const isim = "Namık";
+//       // Dış kapsamdaki değişkene yeniden atama
+//       output = "yeni çıktı";
+//       const str = `${isim} Y kusaginda dogmussun`;
+//       console.log(str);
+//       function ekle(a, b) {
+//         return a + b;
+//       }
+//     }
+//     console.log(output);
+//   }
+//   yasYazdir();
+//   return yas;
+// }
+
+// const isim = "Murat";
+
+// yasHesap(1986);
+
+// --------------------Hoisting & TDZ(Temporal Dead Zone) Başlangıç--------------
+// Değişkenlerde hoisting
+
+// console.log(ben);
+//console.log(iş);
+//console.log(yil);
+
+// var ben = "Murat";
+// let iş = "Öğretmen";
+// const yil = 1986;
+
+// Fonksiyonlarda hoisting
+
+// console.log(ekleme(1,1));
+//console.log(toplama(1+1));
+//console.log(birikim(1+1));
+
+// function ekleme(a, b) {
+//   return a + b;
+// }
+
+// const toplama = function (a, b) {
+//   return a+b
+// };
+
+// const birikim = (a, b) => a + b;
+
+// var x = 1
+// let y =2
+// const z = 3
+
+
+// --------------------Hoisting & TDZ(Temporal Dead Zone) Bitiş--------------
+// --------------------this başlangıç--------------
+
+const murat ={
+  isim:'Murat',
+  yil:1986,
+  yasHesap: function () {
+    return 2040 - this.yil
+  },
 }
 
-const isim = "Murat";
+console.log(murat.yasHesap());
 
-yasHesap(1986);
+
+// --------------------this bitiş--------------
